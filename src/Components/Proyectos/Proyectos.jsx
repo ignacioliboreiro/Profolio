57d6f9c0916db12f723f898d239942e1016572b0
ignacio liboreiro <ignaliboreiro@gmail.com>
2024-06-06 17:14:16 -0300

    iniciando proyecto

--- src/Components/Proyectos/Proyectos.jsx
+++ src/Components/Proyectos/Proyectos.jsx
@@ -2,9 +2,9 @@
 import "./Proyectos.css"
 const Proyectos = () => {
     return (
-        <div>
+        <div className="proyectos">
             <h1 id="Proyecto">Proyectos</h1>
-            <div>
+            <div className="proyectos">
                 <a href="https://pf-liboreiro.vercel.app/index.html" target="_blank">
                 <img src="../public/img/sneakers.jpg" alt="Proyecto Sneaker"/>
                 </a>
